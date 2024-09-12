ba1d5cf06cfb66a02f3b826d781497e1ee19a664
Ljblack1 <152862088+Ljblack1@users.noreply.github.com>
2024-09-12 07:02:41 -0700

    Add files via upload

--- sportybet/aviator.js
+++ sportybet/aviator.js
@@ -1,96 +1,58 @@
-const moves = ["2.33", "5.67", "1.20", "13.55"];
+let moves = ["2.33", "5.67", "1.20", "13.55"];
 
 
 const loadingBar = document.querySelector('.loading-bar');
 const loadingFill = document.querySelector('.loading-bar-fill');
 const btn = document.querySelector('.nxt-btn');
-const move = document.querySelector('.value');
 const pending = document.querySelector('.pending');
-const loader = document.querySelector('.small-load');
-const time = document.querySelector('.time');
+const loader = document.querySelector('.loader');
+const bgWithSignal = document.querySelector('.bg-section-two');
+const sLoader = document.querySelector('.small-load');
+const move = document.querySelector('.value');
 
 const loginContainer = document.querySelector('.login-container');
 
-// preloader before site open
-const starBtns3 = document.querySelectorAll('#preloader3 .stars .star-btn');
-const starBtns4 = document.querySelectorAll('#preloader3 .stars2 .star-btn');
+// working on adding moves
+// const openMove = document.querySelector('.header');
+// const moveContainer = document.querySelector('.add-moves-contaner');
+// const closeMove = document.querySelector('.close-move-btn');
+// const addMoveBtn = document.querySelector('.add-move-btn');
+// const moveInputs = document.querySelectorAll('.add-moves-input');
 
-function changeColor3(target) {
-  if (target < 1) {
-    starBtns3[0].style.color = "red";
-  } else if (target < 2) {
-    starBtns3[0].style.color = "red";
-    starBtns3[1].style.color = "red";
-  } else if (target < 3) {
-    starBtns3[0].style.color = "red";
-    starBtns3[1].style.color = "red";
-    starBtns3[2].style.color = "red";
-  } else if (target < 4) {
-    starBtns3[0].style.color = "red";
-    starBtns3[1].style.color = "red";
-    starBtns3[2].style.color = "red";
-    starBtns3[3].style.color = "red";
-  } else {
-    starBtns3[0].style.color = "red";
-    starBtns3[1].style.color = "red";
-    starBtns3[2].style.color = "red";
-    starBtns3[3].style.color = "red";
-    starBtns3[4].style.color = "red";
-  }
-}
-function changeColor4(target) {
-  if (target < 1) {
-    starBtns4[0].style.color = "red";
-  } else if (target < 2) {
-    starBtns4[0].style.color = "red";
-    starBtns4[1].style.color = "red";
-  } else if (target < 3) {
-    starBtns4[0].style.color = "red";
-    starBtns4[1].style.color = "red";
-    starBtns4[2].style.color = "red";
-  } else if (target < 4) {
-    starBtns4[0].style.color = "red";
-    starBtns4[1].style.color = "red";
-    starBtns4[2].style.color = "red";
-    starBtns4[3].style.color = "red";
-  } else {
-    starBtns4[0].style.color = "red";
-    starBtns4[1].style.color = "red";
-    starBtns4[2].style.color = "red";
-    starBtns4[3].style.color = "red";
-    starBtns4[4].style.color = "red";
-  }
-}
+// // open move container
+// openMove.addEventListener('click', () => {
+//   moveContainer.classList.add('show-move-container');
+// })
+// // closing move container
+// closeMove.addEventListener('click', () => {
+//   moveContainer.classList.remove('show-move-container');
+// })
 
-starBtns3.forEach((starBtn3) => {
-  starBtn3.addEventListener('click', (e) => {
-    const target = e.currentTarget.dataset.id;
-    changeColor3(target);
-  })
-  setTimeout(() => {
-    preloader3.style.display = "none";
-    loginContainer.style.display = "flex";
-  }, 5000)
-})
-
-starBtns4.forEach((starBtn4) => {
-  starBtn4.addEventListener('click', (e) => {
-    const target = e.currentTarget.dataset.id;
-    changeColor4(target);
-  })
-})
+// addMoveBtn.addEventListener('click', () => {
+//   const movesValues = moveInputs;
+//   moves = [];
+//   movesValues.forEach((movesValue) => {
+//     moves.push(movesValue.value);
+//     movesValue.value = '';
+//   })
+//   moveContainer.classList.remove('show-move-container');
+//   localStorage.setItem("moves", JSON.stringify(moves));
+// })
 
 // working on the login
 const input = document.getElementById('id');
 const form = document.querySelector('.login');
 // const sBtn = document.querySelector('.submit-btn');
+// preloader
+
+const preloder1 = document.querySelector('.preloader');
 
 const alert = document.querySelector('.alert');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const id = input.value;
-  loader.style.display = "flex";
+  sLoader.style.display = "flex";
 
   if (id && id === '#pro530#') {
     // displayAlert('succeded please wait', 'success');
@@ -98,12 +60,16 @@ form.addEventListener('submit', (e) => {
       loginContainer.style.transform = `translateX(${-100}%)`;
       loginContainer.style.transition = `all ${1}s ease`;
       // loginContainer.style.display = "none";
-      loader.style.display = "none";
-      loader.style.transition = `all ${1}s ease`;
+      sLoader.style.display = "none";
+      sLoader.style.transition = `all ${1}s ease`;
+      preloder1.style.display = "flex";
+      setTimeout(() => {
+        preloder1.style.display = "none";
+      }, 8000)
     }, 5000)
   } else {
     setTimeout(() => {
-      loader.style.display = "none";
+      sLoader.style.display = "none";
       displayAlert('invalid id', 'danger');
     }, 3000)
   }
@@ -124,68 +90,51 @@ function displayAlert(text, action) {
 
 // working on the login ends here
 
-// preloader
 
-// const preloder1 = document.querySelector('.preloader');
+loginContainer.style.display = "flex";
 
 // window.addEventListener('DOMContentLoaded', () => {
-//   preloder1.style.display = "flex";
+//   move.style.display = "none"; 
 // })
-
-// window.addEventListener('load', () => {
-//   setTimeout(() => {
-//     preloder1.style.display = "none";
-//   }, 5000)
-// })
-
-
-function getTime() {
-  let hours = new Date().getHours();
-  let minutes = new Date().getMinutes();
-
-  minutes < 10 ? minutes = `0${minutes}` : minutes = minutes;
-
-  if (hours > 12) {
-    hours = hours - 12;
-    return `${hours}:${minutes} PM`;
-  } else {
-    return `${hours}:${minutes} AM`;
-  }
-
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  time.textContent = getTime();
-  
-  setInterval(() => {
-    time.textContent = getTime();
-  }, 1000)
-})
 
 let value = 0;
 
 btn.addEventListener("click", () => {
   
-  loader.style.display = "flex";
+  sLoader.style.display = "flex";
+
+  bgWithSignal.style.transform = `scaleY(${1.2}) translateX(${100}%)`;
+  
+  setTimeout(() => {
+    // btn.textContent = "Next Game";
+    sLoader.style.display = "none";
+    bgWithSignal.style.transform = `scaleY(${1.2}) translateX(${0})`;
+  }, 15000)
+
 
   value++;
 
-  if (value > moves.length -1) {
-    value = 0;
-  }
+  // if (value > moves.length -1) {
+  //   value = 0;
+  // }
+  
+  // loader.style.transform = `rotate(${1440}deg)`;
+  // loader.style.transition = `all ${2}s ease`;
+  
+  loadingFill.style.width = `${0}%`;
+  
   setTimeout(() => {
-    // loadingFill.style.transform = `translateX(${100}%)`;
-    // btn.textContent = "Next";
-    // loader.style.display = "none";
-    // move.textContent = `${moves[value]}x`;
+    pending.style.display = "none";
+    
+    // loader.style.transform = `rotate(${0}deg)`;
+    // loader.style.transition = `all ${0}s ease`;
+    
+    // move.style.display = "block"; 
+    
+    loadingFill.style.width = `${100}%`;
 
-    loader.innerHTML = `
-      <p id="error">Error</p>
-      <p>Please try connicting to VPN</p>
-    `;
+    // move.textContent = `${moves[value]}`;
+  }, 3000);
 
-    // loader.style.display = "flex";
-  }, 20000);
-
-  loadingFill.style.transform = `translateX(${-100}%)`;
+  // loadingFill.classList.add("hide-fill");
 })
